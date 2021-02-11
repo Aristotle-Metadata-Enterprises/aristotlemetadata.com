0aa114c8f16fcaeecbe25f85db913afc6dc234b7
Samuel Spencer <theodore.therone@gmail.com>
2021-02-11 22:38:16 +0000

    opps

--- src/aristotle/babies.js
+++ src/aristotle/babies.js
@@ -1,51 +1,42 @@
   var promo_strings = [
-    "healthier communities", //babies",
+    "build healthier communities", //babies",
     //"higher living standards",
-    "safer cities", // "lower crime",
-    "gender equality",
-    "sustainable cities",
-    "a cleaner environment",
-    "positive outcomes"
+    "build safer cities", // "lower crime",
+    "achieve gender equality",
+    "build sustainable cities",
+    "create a cleaner environment",
+    "achieve positive outcomes",
+    "do more!"
   ];
+var shuffle = true;
   if (window.location.hash == "#derek") {
       promo_strings = [
-        "Children Who Can Read Good And Wanna Learn To Do Other Stuff Good Too"
+        "teach Kids Who Can't Read Good and Who Wanna Learn to Do Other Stuff Good Too"
       ];
   } else if (window.location.hash == "#rufus") {
     promo_strings = [
-      "cleaner air",
-      "cleaner water",
-      "cleaner dirt",
-      "higher bowling averages",
-      "lower mini-golf scores",
-      "more excellent water-slides", 
-      "most excellent adventures",
-      "a better San Dimas"
+      "get cleaner air",
+      "get cleaner water",
+      "get cleaner dirt",
+      "get higher bowling averages",
+      "get lower mini-golf scores",
+      "get more excellent water-slides", 
+      "have most excellent adventures",
+      "have a better San Dimas",
     ];
+    shuffle = false;
   }
-  $(function(){
-    $(".healthy-babies span").typed({
-      strings: promo_strings,
-      startDelay: 3000,
-      typeSpeed: 50,
-      loop: true,
-      showCursor: true,
-      backDelay: 3000,
-      backSpeed: 50,
-      cursorChar: "_",
-    });
-  });
-  
       
 $(document).ready(function() {
-      /* ======= Header Background Slideshow - Flexslider ======= */    
-      /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties * /
-      
-      $('.bg-slider').flexslider({
-          animation: "fade",
-          directionNav: false, //remove the default direction-nav - https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
-          controlNav: false, //remove the default control-nav
-          slideshowSpeed: 8000,
-          useCSS: false,
-      }); */
+  var typed = new Typed(".healthy-babies span", {
+    strings: promo_strings,
+    startDelay: 2000,
+    typeSpeed: 40,
+    loop: true,
+    showCursor: true,
+    backDelay: 2000,
+    backSpeed: 40,
+    shuffle: shuffle,
+    cursorChar: "|",
+  });
 });
