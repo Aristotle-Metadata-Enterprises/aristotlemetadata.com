1ed801edf8e13458d563056f0b28089aa37310a3
Samuel Spencer <theodore.therone@gmail.com>
2021-02-16 10:21:28 +0000

    add final images

--- src/aristotle/babies.js
+++ src/aristotle/babies.js
@@ -1,9 +1,9 @@
   var promo_strings = [
     "build healthier communities.",
     //"higher living standards.",
-    "reduce the cost of business",
+    "reduce the cost of business.",
     "build safer cities.",
-    "achieve gender equality",
+    "achieve gender equality.",
     "build sustainable cities.",
     "create a cleaner environment.",
     "achieve positive outcomes.",
@@ -12,18 +12,18 @@
 var shuffle = true;
   if (window.location.hash == "#derek") {
       promo_strings = [
-        "teach Kids Who Can't Read Good and Who Wanna Learn to Do Other Stuff Good Too"
+        "teach Kids Who Can't Read Good and Who Wanna Learn to Do Other Stuff Good Too."
       ];
   } else if (window.location.hash == "#rufus") {
     promo_strings = [
-      "get cleaner air",
-      "get cleaner water",
-      "get cleaner dirt",
-      "get higher bowling averages",
-      "get lower mini-golf scores",
-      "get more excellent water-slides", 
-      "have most excellent adventures",
-      "have a better San Dimas",
+      "get cleaner air.",
+      "get cleaner water.",
+      "get cleaner dirt.",
+      "get higher bowling averages.",
+      "get lower mini-golf scores.",
+      "get more excellent water-slides.", 
+      "have most excellent adventures.",
+      "have a better San Dimas.",
     ];
     shuffle = false;
   }
